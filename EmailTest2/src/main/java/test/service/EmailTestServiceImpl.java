9eb402d762d12f5a0cd6a2c6a631ec345546d1a3
Vladimir <v_litva@mail.ru>
2020-12-05 14:51:27 +0200

    test

--- EmailTest2/src/main/java/test/service/EmailTestServiceImpl.java
+++ EmailTest2/src/main/java/test/service/EmailTestServiceImpl.java
@@ -4,6 +4,7 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -22,16 +23,19 @@ public class EmailTestServiceImpl implements EmailTestService {
 
 	@Override
 	public void sendEmail() {
-		MimeMessage message = mailSender.createMimeMessage();
-		MimeMessageHelper helper;
-		String htmlBlock = "<html><body><img src='https://pbs.twimg.com/profile_images/758084549821730820/_HYHtD8F.jpg'></body></html>";
-		try {
-			helper = new MimeMessageHelper(message, true);
-			helper.setTo("v.litva.alf@gmail.com");
-			helper.setText(htmlBlock, true);
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
+//		MimeMessage message = mailSender.createMimeMessage();
+//		MimeMessageHelper helper;
+//		String htmlBlock = "<html><body><img src='https://pbs.twimg.com/profile_images/758084549821730820/_HYHtD8F.jpg'></body></html>";
+//		try {
+//			helper = new MimeMessageHelper(message, true);
+//			helper.setTo("v.litva.alf@gmail.com");
+//			helper.setText(htmlBlock, true);
+//		} catch (MessagingException e) {
+//			e.printStackTrace();
+//		}
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo("v.litva.alf@gmail.com");
+		message.setText("Test");
 		mailSender.send(message);
 		Email email = Email.builder().build();
 		repository.save(email);
